58473f7e1de123eed36a1c3eb89700322d147332
Taranukha-Kostiantyn <124849285+Taranukha-Kostiantyn@users.noreply.github.com>
2023-03-07 10:52:02 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+$('.bike-slider,.slider__items').slick({
+  arrows:false,
+  dots:true,
+  fade:true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+})
+
+});
+
+//$(".menu, .bike__column").on("click","a", function(event){
+//  event.preventDefault();
+//  var id=$(this).attr('href'),
+//  top =$(id).offset().top;
+//  $('body,html').animate({skrollTop: top},1500);
+//});
